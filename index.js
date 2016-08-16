7b5353c9570f5ff464fbae96eeed3aee01456168
Tong Shen <tshen@farseerinc.com>
2016-08-16 14:49:19 -0400

    Use native fetch for Android. Bump version to v0.1.3.

--- index.js
+++ index.js
@@ -5,40 +5,45 @@
 
 import {NativeModules} from 'react-native';
 const NativeSimpleFetch = NativeModules.SimpleFetch;
+let newFetch = fetch;
 
-const fetch = function (url, options) {
-    const params = {
-        url: url,
-        method: options.method ? options.method.toUpperCase() : 'GET',
-        headers: options.headers,
-        timeout: 10000,
-        body: options.body,
-        gzipRequest: true
-    };
-
-    return NativeSimpleFetch.sendRequest(params).then((res)=> {
-        const status = parseInt(res[0]);
-        const body = res[1];
-        return {
-            ok: status >= 200 && status < 300,
-            status: status,
-            json: ()=> {
-                return new Promise((resolve, reject)=> {
-                    try {
-                        let obj = JSON.parse(body);
-                        resolve(obj);
-                    } catch (e) {
-                        reject(e);
-                    }
-                });
-            },
-            text: ()=> {
-                return Promise.resolve(body);
-            }
+if (NativeSimpleFetch) {
+    newFetch = function (url, options) {
+        const params = {
+            url: url,
+            method: options.method ? options.method.toUpperCase() : 'GET',
+            headers: options.headers,
+            timeout: 10000,
+            body: options.body,
+            gzipRequest: true
         };
-    });
-};
+
+        return NativeSimpleFetch.sendRequest(params).then((res)=> {
+            const status = parseInt(res[0]);
+            const body = res[1];
+            return {
+                ok: status >= 200 && status < 300,
+                status: status,
+                json: ()=> {
+                    return new Promise((resolve, reject)=> {
+                        try {
+                            let obj = JSON.parse(body);
+                            resolve(obj);
+                        } catch (e) {
+                            reject(e);
+                        }
+                    });
+                },
+                text: ()=> {
+                    return Promise.resolve(body);
+                }
+            };
+        });
+    };
+}
+
+
 
 module.exports = {
-    fetch
+    fetch: newFetch
 };
